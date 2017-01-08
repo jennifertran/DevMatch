61be6a49a43bb753cdd75b8d565860100612eff8
Jennifer Tran <jennifer.tran101@gmail.com>
2017-01-08 04:12:38 +0000

    Added comments

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Actually send the email
+# Actually send the email via simple mail transfer protocol
 ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
