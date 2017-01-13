1d452b8873215728153e0d0ff6345d7d72c7873f
Jennifer Tran <jennifer.tran101@gmail.com>
2017-01-13 02:57:19 +0000

    Added UI msg for users to sign up and favicon

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   
+  # Only do this when the user is signing up
+  before_action :select_plan, only: :new
+  
   # Extends the default Devisor behaviour so that
   # users who are signing up with the pro account is
   # saved with the special stripe function otherwise,
@@ -22,4 +25,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
     
   end
+  
+  
+  private
+    # Forces the user to select either one of the two plans if they 
+    # decide to mess around with the url plans
+    def select_plan
+      # Do not do this while the user is selecting plan 1 or 2
+      unless (params[:plan] == '1' || params[:plan] == '2')
+        # Rediect the user to the homepage with a warning
+        flash[:notice] = "Please select a membership plan to sign up."
+        redirect_to root_url
+      end
+    end
 end
