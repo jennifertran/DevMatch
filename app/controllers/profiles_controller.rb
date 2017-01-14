82f2eb46dc2aa17aa99eb22b38e4298a10b59a42
Jennifer Tran <jennifer.tran101@gmail.com>
2017-01-14 23:03:26 +0000

    Added profiles create action, user controller, and user show view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,4 +7,30 @@ class ProfilesController < ApplicationController
       @profile = Profile.new
     end
     
+    # Save it to the profile DB
+    # POST to /users/:user_id/profile
+    def create
+      # Ensure that we have the user who is filling out form
+      @user = User.find(params[:user_id])
+      
+      # This will link the profile to the user that is logged in
+      @profile = @user.build_profile(profile_params)
+      
+      # Show message is profile is updated successfully
+      if @profile.save
+        flash[:success] = "Profile updated!"
+        redirect_to root_path
+      else
+        # Refresh the page with a blank form
+        render action: :new
+      end
+    end
+    
+    # Whitelist the form fields to prevent hackers from
+    # adding additional form fields
+    private
+      def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      end
+      
 end
