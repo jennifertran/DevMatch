43e2c042b11f3c167bc78e30386b547af3baa09f
Jennifer Tran <jennifer.tran101@gmail.com>
2017-01-14 05:08:27 +0000

    Added profiles controller, view, and link on homepage

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,9 @@
+class ProfilesController < ApplicationController
+    
+    # Makes takes user to a blank profile form
+    # GET to /users/:user_id/profile/new
+    def new
+      # Render blank profile details form
+    end
+    
+end
