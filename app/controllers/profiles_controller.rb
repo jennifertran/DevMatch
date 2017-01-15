7b088322b56ffd8ec4204cc223302ddbdb119f98
Jennifer Tran <jennifer.tran101@gmail.com>
2017-01-15 01:26:56 +0000

    Added avatar column to profiles table and the ability to upload images to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -31,7 +31,7 @@ class ProfilesController < ApplicationController
     # adding additional form fields
     private
       def profile_params
-        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
       end
       
 end
